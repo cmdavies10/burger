f2c140973bcee62ff1c7aaa7701142d8882c7b4c
Chris Davies <c.m.davies10@gmail.com>
2019-11-04 15:09:38 -0500

    connected to burgers db

--- db/schema.sql
+++ db/schema.sql
@@ -2,7 +2,7 @@ DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
---check the boolean case and default values
+/*check the boolean case and default values*/
 CREATE TABLE burgers (
     id int NOT NULL AUTO_INCREMENT,
     burger_name varchar(255) NOT NULL,
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,6 @@
 USE burgers_db;
 
---check boolean value
-INSERT INTO burgers (burger_name, devoured)
-VALUES ("", false)
+/*check boolean value*/
+INSERT INTO burgers (burger_name, devoured) VALUES ("Hamburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Jalapeno Cheddar Burger", false);
