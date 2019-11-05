c6c08179d059ce33304b78774d3a919ab4f797e3
Chris Davies <c.m.davies10@gmail.com>
2019-11-05 10:45:57 -0500

    folder and file structure complete

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+var mysql = require("mysql");
+require("dotenv").config();
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: burgers_db
+});
+
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    } else {
+        console.log("connected as ID: " + connection.threadId);
+    }
+});
+
+module.exports = connection;
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require("express");
-var exphbs = require("express-handlebars");
-var mysql = require("mysql");
+// var exphbs = require("express-handlebars");
+// var mysql = require("mysql");
 
-require("dotenv").config()
+// require("dotenv").config()
 
 var app = express();
 
@@ -14,19 +14,29 @@ app.use(express.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: "burgers_db"
-});
+var routes = require("./controllers/burger_controller.js");
 
-connection.connect(function(err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
+app.use(routes);
 
-    console.log("connected as id " + connection.threadId);
-});
+app.listen(PORT, function() {
+    console.log("Listening at localhost:" + PORT);
+})
+
+// var connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: "burgers_db"
+// });
+
+// connection.connect(function(err) {
+//     if (err) {
+//         console.error("error connecting: " + err.stack);
+//         return;
+//     }
+
+//     console.log("connected as id " + connection.threadId);
+// });
+
+// module.exports = connection;
