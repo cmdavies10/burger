0ce0f6ef98893e0ce57df6dbdadd8b2913471df1
Chris Davies <c.m.davies10@gmail.com>
2019-11-05 13:01:17 -0500

    orm, model plus heroku app created

--- models/burgers.js
+++ models/burgers.js
@@ -2,5 +2,22 @@ var orm = require("../config/orm.js");
 
 // create the code that will call the ORM functions using burger specific input
 // for the ORM.
+var burger = {
+    selectAll: function(cb) {
+        orm.selectAll("burgers", function(res) {
+            cb(res);
+        });
+    },
+    createOne: function(cols, vals, cb) {
+        orm.createOne("burgeres", cols, vals, function(res) {
+            cb(res);
+        });
+    },
+    updateOne: function(objColVals, condition, cb) {
+        orm.updateOne("burgers", objColVals, condition, function() {
+            cb(res);
+        });
+    }
+}
 
 module.exports = burger;
